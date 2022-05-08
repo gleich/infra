e9e27a5dba038c46713535e9f246cae08d62857a
Matt Gleich <git@mattglei.ch>
2022-05-08 15:58:42 -0400

    feat: ghs-games record
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- mattgleich.com.tf
+++ mattgleich.com.tf
@@ -174,3 +174,10 @@ resource "cloudflare_record" "mattgleich_com_daily_poll_website" {
   name    = "daily-poll"
   value   = "76.76.21.21"
 }
+
+resource "cloudflare_record" "mattgleich_com_ghs_games_website" {
+  zone_id = var.zone_id_mattgleich_com
+  type    = "A"
+  name    = "ghs-games"
+  value   = "76.76.21.21"
+}
