bbb9099e3c19bc109da7f29b4bb82d81f6afd9a2
Matt Gleich <git@mattglei.ch>
2024-12-14 23:24:07 -0500

    feat: add blackbird project and website-v2 project
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- modules/vercel/projects.tf
+++ modules/vercel/projects.tf
@@ -6,3 +6,21 @@ resource "vercel_project" "website" {
     type = "github"
   }
 }
+
+resource "vercel_project" "blackbird" {
+  name      = "blackbird"
+  framework = "nextjs"
+  git_repository = {
+    repo = "blackbirdtheme/website"
+    type = "github"
+  }
+}
+
+resource "vercel_project" "website-v2" {
+  name      = "website-v2"
+  framework = "nextjs"
+  git_repository = {
+    repo = "gleich/website-v2"
+    type = "github"
+  }
+}
