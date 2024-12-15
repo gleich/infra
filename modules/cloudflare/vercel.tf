f35043200d177ecc77997162be1676bc283faa44
Matt Gleich <git@mattglei.ch>
2024-12-15 02:40:03 -0500

    feat: newtab records and vercel project
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- modules/cloudflare/vercel.tf
+++ modules/cloudflare/vercel.tf
@@ -14,14 +14,6 @@ resource "cloudflare_record" "www" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "svg_animation_tester" {
-  zone_id = var.zone_id
-  type    = "A"
-  name    = "svg-animation-tester"
-  content = var.addresses.vercel
-  ttl     = 1
-}
-
 resource "cloudflare_record" "blackbird" {
   zone_id = var.zone_id
   type    = "A"
@@ -30,26 +22,10 @@ resource "cloudflare_record" "blackbird" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "feynman_project" {
+resource "cloudflare_record" "newtab" {
   zone_id = var.zone_id
   type    = "A"
-  name    = "feynman-project"
-  content = var.addresses.vercel
-  ttl     = 1
-}
-
-resource "cloudflare_record" "javelin" {
-  zone_id = var.zone_id
-  type    = "A"
-  name    = "javelin"
-  content = var.addresses.vercel
-  ttl     = 1
-}
-
-resource "cloudflare_record" "grad" {
-  zone_id = var.zone_id
-  type    = "A"
-  name    = "grad"
+  name    = "newtab"
   content = var.addresses.vercel
   ttl     = 1
 }
--- modules/vercel/projects.tf
+++ modules/vercel/projects.tf
@@ -7,6 +7,15 @@ resource "vercel_project" "website" {
   }
 }
 
+resource "vercel_project" "newtab" {
+  name      = "website"
+  framework = "sveltekit"
+  git_repository = {
+    repo = "gleich/newtab"
+    type = "github"
+  }
+}
+
 resource "vercel_project" "blackbird" {
   name      = "blackbird"
   framework = "nextjs"
