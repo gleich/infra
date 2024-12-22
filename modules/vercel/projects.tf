fe673f7c128d1b2e0897854c481fef72a5142e1f
Matt Gleich <git@mattglei.ch>
2024-12-21 22:28:36 -0500

    feat: add website-v5 vercel project
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- modules/vercel/projects.tf
+++ modules/vercel/projects.tf
@@ -11,3 +11,12 @@ resource "vercel_project" "blackbird" {
   name      = "blackbird"
   framework = "nextjs"
 }
+
+resource "vercel_project" "website_v5" {
+  name      = "website-v5"
+  framework = "nextjs"
+  git_repository = {
+    repo = "gleich/website-v5"
+    type = "github"
+  }
+}
