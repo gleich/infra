04cb53a8142761a70a16b30762076621d743950f
Matt Gleich <git@mattglei.ch>
2025-02-06 12:58:54 -0500

    feat: update personal droplet size
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- modules/digitalocean/droplets.tf
+++ modules/digitalocean/droplets.tf
@@ -2,6 +2,6 @@ resource "digitalocean_droplet" "personal" {
   image      = "ubuntu-22-04-x64"
   name       = "personal"
   region     = "nyc1"
-  size       = "s-2vcpu-2gb-intel"
+  size       = "s-1vcpu-1gb-intel"
   monitoring = true
 }
