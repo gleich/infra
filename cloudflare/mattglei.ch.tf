a3db5eb43a815d33c5f31f5864c3af61daa9f00c
Matt Gleich <git@mattglei.ch>
2025-05-27 00:25:25 -0400

    feat: proxy traffic for s3 subdomain
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- cloudflare/mattglei.ch.tf
+++ cloudflare/mattglei.ch.tf
@@ -79,7 +79,7 @@ resource "cloudflare_dns_record" "mattgleich_terminal" {
 resource "cloudflare_dns_record" "mattgleich_s3" {
   content = local.lab_address
   name    = "s3.mattglei.ch"
-  proxied = false
+  proxied = true
   ttl     = 1
   type    = "A"
   zone_id = local.mattgleich_zone
