9b8a14a6f163a264a049b6393c97a19556c0f27b
Matt Gleich <git@mattglei.ch>
2026-04-25 18:36:22 -0400

    feat: root gleich.tv record

--- cloudflare/gleich.tv.tf
+++ cloudflare/gleich.tv.tf
@@ -2,6 +2,15 @@ locals {
   gleichtv_zone = "b58fac2767154039d6719c89f3688677"
 }
 
+resource "cloudflare_dns_record" "gleichtv" {
+  content = local.lab_address
+  name    = "gleich.tv"
+  proxied = false
+  ttl     = 1
+  type    = "A"
+  zone_id = local.gleichtv_zone
+}
+
 resource "cloudflare_dns_record" "gleichtv_stream" {
   content = local.lab_address
   name    = "stream.gleich.tv"
